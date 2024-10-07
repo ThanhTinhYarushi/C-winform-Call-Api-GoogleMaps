0c17e9d5411ffad123e481da2a07de847af8abc7
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-10-07 19:15:33 +0700

    Base

--- MainProject/Form1.Designer.cs
+++ MainProject/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_DiemDi = new System.Windows.Forms.TextBox();
+            this.btn_Tim = new System.Windows.Forms.Button();
+            this.txt_DiemDen = new System.Windows.Forms.TextBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // txt_DiemDi
+            // 
+            this.txt_DiemDi.Location = new System.Drawing.Point(12, 12);
+            this.txt_DiemDi.Name = "txt_DiemDi";
+            this.txt_DiemDi.Size = new System.Drawing.Size(100, 20);
+            this.txt_DiemDi.TabIndex = 1;
+            // 
+            // btn_Tim
+            // 
+            this.btn_Tim.Location = new System.Drawing.Point(127, 25);
+            this.btn_Tim.Name = "btn_Tim";
+            this.btn_Tim.Size = new System.Drawing.Size(75, 23);
+            this.btn_Tim.TabIndex = 2;
+            this.btn_Tim.Text = "Tìm";
+            this.btn_Tim.UseVisualStyleBackColor = true;
+            this.btn_Tim.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // txt_DiemDen
+            // 
+            this.txt_DiemDen.Location = new System.Drawing.Point(12, 38);
+            this.txt_DiemDen.Name = "txt_DiemDen";
+            this.txt_DiemDen.Size = new System.Drawing.Size(100, 20);
+            this.txt_DiemDen.TabIndex = 3;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 85);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(1074, 526);
+            this.gMapControl1.TabIndex = 4;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1098, 623);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.txt_DiemDen);
+            this.Controls.Add(this.btn_Tim);
+            this.Controls.Add(this.txt_DiemDi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_DiemDi;
+        private System.Windows.Forms.Button btn_Tim;
+        private System.Windows.Forms.TextBox txt_DiemDen;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
     }
 }
 
--- MainProject/Form1.cs
+++ MainProject/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +10,117 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET.WindowsForms.Markers;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MainProject
 {
     public partial class Form1 : Form
     {
+        private GMapOverlay markersOverlay;
+        private GraphHopperService graphHopperService;
+        private string apiKey = "88d49a17-f0ca-4b91-b3fc-e0d9e04e7676";
+
+
         public Form1()
         {
             InitializeComponent();
+
+            // Cấu hình GMapControl
+            gMapControl1.MapProvider = GMapProviders.OpenStreetMap;
+            gMapControl1.Position = new PointLatLng(48.858844, 2.294351); // Paris, Eiffel Tower
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 20;
+            gMapControl1.Zoom = 13;
+            gMapControl1.ShowCenter = false;
+
+            // Tạo lớp overlay cho các marker và tuyến đường
+            markersOverlay = new GMapOverlay("markers");
+            gMapControl1.Overlays.Add(markersOverlay);
+        }
+
+        private async void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Lấy tọa độ từ TextBox
+            string origin = txt_DiemDi.Text; // Ví dụ: "48.858844,2.294351"
+            string destination = txt_DiemDen.Text; // Ví dụ: "48.8566,2.3522"
+
+            try
+            {
+                // Gọi API để tính toán lộ trình
+                var route = await GetRouteFromGraphHopper(origin, destination);
+
+                if (route != null)
+                {
+                    // Hiển thị tuyến đường lên bản đồ
+                    DrawRoute(route);
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy lộ trình.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Có lỗi xảy ra: {ex.Message}");
+            }
+        }
+        private async Task<PointLatLng[]> GetRouteFromGraphHopper(string origin, string destination)
+        {
+            string url = $"https://graphhopper.com/api/1/route?point={origin}&point={destination}&vehicle=car&points_encoded=false&key={apiKey}";
+
+            using (var client = new HttpClient())
+            {
+                var responseMessage = await client.GetAsync(url);
+                if (!responseMessage.IsSuccessStatusCode)
+                {
+                    throw new Exception("Failed to get route from GraphHopper");
+                }
+
+                var response = await responseMessage.Content.ReadAsStringAsync();
+                dynamic jsonResponse = JsonConvert.DeserializeObject(response);
+
+                if (jsonResponse.paths != null && jsonResponse.paths.Count > 0)
+                {
+                    var path = jsonResponse.paths[0].points.coordinates;
+                    PointLatLng[] routePoints = new PointLatLng[path.Count];
+
+                    for (int i = 0; i < path.Count; i++)
+                    {
+                        var point = path[i];
+                        routePoints[i] = new PointLatLng((double)point[1], (double)point[0]); // Đảo ngược lat/lon
+                    }
+
+                    return routePoints;
+                }
+
+                return null;
+            }
+        }
+
+        // Hàm vẽ lộ trình lên bản đồ
+        private void DrawRoute(PointLatLng[] routePoints)
+        {
+            // Xóa các tuyến đường và marker cũ
+            markersOverlay.Routes.Clear();
+            markersOverlay.Markers.Clear();
+
+            // Thêm marker cho điểm bắt đầu
+            var startMarker = new GMarkerGoogle(routePoints[0], GMarkerGoogleType.green_dot);
+            markersOverlay.Markers.Add(startMarker);
+
+            // Thêm marker cho điểm kết thúc
+            var endMarker = new GMarkerGoogle(routePoints[routePoints.Length - 1], GMarkerGoogleType.red_dot);
+            markersOverlay.Markers.Add(endMarker);
+
+            // Vẽ tuyến đường
+            var route = new GMapRoute(routePoints, "route");
+            route.Stroke = new Pen(System.Drawing.Color.Blue, 3); // Đặt màu và độ rộng của đường
+            markersOverlay.Routes.Add(route);
+
+            // Tự động zoom map để hiển thị toàn bộ tuyến đường
+            gMapControl1.ZoomAndCenterMarkers("markers");
         }
     }
 }
